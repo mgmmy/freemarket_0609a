9480faa95494c87c04f00617669d9f5b39a67724
mgmmy <mgmilk.u@gmail.com>
2019-06-22 19:02:02 +0900

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "products#index"
   resources :products, only: [:show]
-<<<<<<< HEAD
-<<<<<<< HEAD
   resources :users, only: [:new, :show]
-=======
-  resources :users
->>>>>>> master
-=======
-  resources :users
->>>>>>> master
 
 end

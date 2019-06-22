9979dbf2d3951b7156579f93d0e0ad8a7a0c0a13
TakkAlGhul <takuro.mike@gmail.com>
2019-06-22 21:11:07 +0900

    fixed-details

--- app/assets/javascripts/responsive-sidebar.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-   $(window).on("load resize", init);
-   function init() {
-    var _width = $(window).width();
-    if(_width <= 1067){
-      $('.sidebar').insertAfter('.mypage');
-    }
-  }
-});

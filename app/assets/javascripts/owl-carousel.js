10ea641e2c38e7fa31c579643e2a6255d5ddc1b8
TakkAlGhul <takuro.mike@gmail.com>
2019-06-25 22:54:36 +0900

    create item detail

---
+++ app/assets/javascripts/owl-carousel.js
@@ -0,0 +1,42 @@
+$(function(){
+  $(".owl-carousel").owlCarousel({  
+    items: 1,
+  });
+}); 
+
+$(function() {
+	 
+  dotcount = 1;
+
+  $('.owl-dot').each(function() {
+    $( this ).addClass( 'dotnumber' + dotcount);
+    $( this ).attr('data-info', dotcount);
+    dotcount=dotcount+1;
+  });
+  
+  slidecount = 1;
+
+  $('.owl-item').not('.cloned').each(function() {
+    $( this ).addClass( 'slidenumber' + slidecount);
+    slidecount=slidecount+1;
+  });
+  
+  $('.owl-dot').each(function() {
+  
+      grab = $(this).data('info');
+
+      slidegrab = $('.slidenumber'+ grab +' img').attr('src');
+      console.log(slidegrab);
+
+      $(this).css("background-image", "url("+slidegrab+")");  
+
+  });
+  
+  amount = $('.owl-dot').length;
+  gotowidth = 100/amount;
+  
+  $('.owl-dot').css("width", gotowidth+"%");
+  newwidth = $('.owl-dot').width();
+  $('.owl-dot').css("height", newwidth+"px");
+  	
+});

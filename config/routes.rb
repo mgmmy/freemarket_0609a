e7d8c31f2697220ff3b80c1ccc481780e0b3dd21
kahoshibuya <a14140.ks@gmail.com>
2019-06-26 15:04:22 +0900

    edit route.rb
    ルートの記載

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-
-  devise_for :users, controllers: {
-    registrations: 'users/registrations',
-    sessions: 'users/sessions'
-  }
   
   root "products#index"
   resources :products, only: [:show]
   resources :users, only: [:new, :show] do
     collection do
       get 'identification' 
+      get 'information' 
+      get 'phonemumber' 
+      get 'address' 
+      get 'howtopay' 
+      get 'complete'
+  
     end
   end
-  
 end

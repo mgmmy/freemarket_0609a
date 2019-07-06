9264ecc710076365f5cb8ec77ef8a630e9d0abc8
mgmmy <mgmilk.u@gmail.com>
2019-07-06 15:14:57 +0900

    Add products new.html.haml

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root "products#index"
-  resources :products, only: [:show]
+  resources :products, only: [:show, :new]
   resources :users, only: [:new, :show] do
     collection do
       get 'identification' 

e98bfcd5c3f26137eb08afb05badc9fbe8abbed0
kahoshibuya <a14140.ks@gmail.com>
2019-07-21 17:04:04 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  get 'card/new'
+
+  get 'card/show'
+
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'

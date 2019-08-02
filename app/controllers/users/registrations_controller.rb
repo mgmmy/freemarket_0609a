b312eb5de0baf257c30aaf6c8d8e578d55fba95a
kahoshibuya <a14140.ks@gmail.com>
2019-08-02 19:24:45 +0900

    update

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,11 +2,20 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   def new
-    @user = User.new
-    @profile = @user.build_profile
+    user = User.new
   end
   
+
+
   def create
+    current_user.user.update(profile_params)
+    binding.pry
     super and return
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:tel,:first_name, :last_name, :first_name_kana, :last_name_kana)
   end
 end

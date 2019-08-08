3a19158f6a91b2f783eabe27769e6c82f39f7f22
kahoshibuya <a14140.ks@gmail.com>
2019-08-08 12:20:23 +0900

    update

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,18 +6,18 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
   
   
-def create
-  @user = User.new(user_params)
-  if @user.save
-    session[:user_id] = @user.id
-    redirect_to phonemumber_users_path
-  else
-    render 'new'
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to phonemumber_users_path
+    else
+      render 'new'
+    end
   end
-end
 
-private
+  private
     def user_params
       params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :last_name, :first_name, :first_name_kana, :last_name_kana)
     end
-  end
+end

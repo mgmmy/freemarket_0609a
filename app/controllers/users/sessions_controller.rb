eb289e9dce05c77d74e618d0df9437251715eb1f
kahoshibuya <a14140.ks@gmail.com>
2019-08-10 15:47:32 +0900

    update

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,19 +1,4 @@
 class Users::SessionsController < Devise::SessionsController
 
-  
-  # def index
-  #     session[:user_name]
-  #     @notice = "#{session[:user_name]}でログインしています。"
-  #     redirect_to phonemumber_users_path
-  #   end
-
-  # if params.key?(:name) || params.key?(:password)
-  #   user = User.find_by_name(params[:name])
-  #   if user && user.authenticate(params[:password])
-  #     session[:user_name] = params[:name]
-  #   else
-  #     session[:user_name] = nil
-  #   end
-  # end
 end
 

ac52a66d6a1e81008d133ef015e5545bed7fd8cb
kahoshibuya <a14140.ks@gmail.com>
2019-08-12 12:19:32 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  get 'purchase/index'
+
+  get 'purchase/done'
+
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations:      'users/registrations',

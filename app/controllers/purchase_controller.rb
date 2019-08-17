4cf3d40c27adef4307df8a1b759eb4e61fb8a06e
TakkAlGhul <takuro.mike@gmail.com>
2019-08-17 17:48:29 +0900

    update

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -12,7 +12,7 @@ class PurchaseController < ApplicationController
   end
 
   def pay
-    @card = Card.where(user_id: session[:user_id]).first
+    @card = Card.find_by(user_id: session[:user_id])
     product = Product.find(params[:product_id])
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     charge = Payjp::Charge.create(

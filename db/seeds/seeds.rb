795db68987042956ae37dba04868cb98a501b3bc
TakkAlGhul <takuro.mike@gmail.com>
2019-08-19 18:39:08 +0900

    update seeds

--- db/seeds/seeds.rb
+++ db/seeds/seeds.rb
@@ -1,20 +1,26 @@
-load(Rails.root.join('db', 'seeds', "#{Rails.env.downcase}.rb"))
-
-
-
-require 'faker/japanese'
-
-50.times do |n|
-  gimei = Gimei.new
-  User.create(
-    nickname: Faker::Japanese::Name.name,
-    email: Faker::Internet.email,
-    password: Faker::Internet.password,
-    first_name: gimei.first.kanji,
-    first_name_kana: gimei.first.hiragana,
-    last_name: gimei.last.kanji,
-    last_name_kana: gimei.last.hiragana,
-    birthday: Faker::Time.between(40.years.ago, 18.years.ago, :all).to_s[0, 10],
-    tel: Faker::PhoneNumber.phone_number
-  )
+table_names = %w(members)
+table_names.each do |table_name|
+  path = Rails.root.join("db/seeds", table_name + ".rb")
+  if File.exist?(path)
+    puts "Creating #{table_name}..."
+    require path
+  end
 end
+
+
+# require 'faker/japanese'
+
+# 50.times do |n|
+#   gimei = Gimei.new
+#   User.create(
+#     nickname: Faker::Japanese::Name.name,
+#     email: Faker::Internet.email,
+#     password: Faker::Internet.password,
+#     first_name: gimei.first.kanji,
+#     first_name_kana: gimei.first.hiragana,
+#     last_name: gimei.last.kanji,
+#     last_name_kana: gimei.last.hiragana,
+#     birthday: Faker::Time.between(40.years.ago, 18.years.ago, :all).to_s[0, 10],
+#     tel: Faker::PhoneNumber.phone_number
+#   )
+# end

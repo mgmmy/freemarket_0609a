79e49ad6b18255c039c6f8d80721681e105fd084
XIAO100he <xiao100he@gmil.com>
2019-08-20 19:20:15 +0900

    Migrationファイルを変更

--- db/migrate/20190728073946_remove_columns_from_products4.rb
+++ db/migrate/20190728073946_remove_columns_from_products4.rb
@@ -1,6 +1,6 @@
 class RemoveColumnsFromProducts4 < ActiveRecord::Migration[5.0]
   def change
-    add_column :products, :charge_id, :integer
+    # add_column :products, :charge_id, :integer
     add_column :products, :condition_id, :integer
     add_column :products, :deliver_method_id, :integer
     add_column :products, :shipment_id, :integer

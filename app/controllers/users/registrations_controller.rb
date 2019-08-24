46072ba8f1dafa562648f736d638672b80fd0d6e
kahoshibuya <a14140.ks@gmail.com>
2019-08-24 14:51:18 +0900

    update

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,7 +5,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   
   
   def create
+    binding.pry
     @user = User.new(user_params)
+    
     if @user.save
       sign_in(@user)
       session[:user_id] = @user.id
@@ -17,6 +19,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   private
     def user_params
-      params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :last_name, :first_name, :first_name_kana, :last_name_kana)
+      params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :last_name, :first_name, :first_name_kana, :last_name_kana, :birthday(1i), birthday(2i), birthday(3i))
     end
 end

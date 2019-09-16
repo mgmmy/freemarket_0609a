80c52a5c68558c072fb4035f32a878d8dc92af94
kahoshibuya <a14140.ks@gmail.com>
2019-09-16 10:40:53 +0900

    update

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,5 +5,7 @@ class Profile < ApplicationRecord
   belongs_to :user
   has_one :prefecture
   has_many :purchases
+
+  validates :postalcode, :city, :block, :tel, presence: true
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
   has_many :purchases
   has_many :sns_credentials, dependent: :destroy
   
-  # validates :nickname, :encrypted_password, :last_name, :first_name, :last_name_kana, :first_name_kana
+  validates :nickname, :encrypted_password, :last_name, :first_name, :last_name_kana, :first_name_kana, presence: true
 
   def self.find_oauth(auth)
     uid = auth.uid
